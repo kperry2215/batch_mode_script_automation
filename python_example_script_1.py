3d2f73f1888c3db53e3bf3285b6f6a91e6416f25
Kirsten Perry <51889148+kperry2215@users.noreply.github.com>
2019-06-21 02:22:27 -0500

    Update python_example_script_1.py

--- python_example_script_1.py
+++ python_example_script_1.py
@@ -7,7 +7,7 @@ def main():
     #Create a dummy pandas dataframe 
     dummy_df=pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
     #Write the dataframe to a csv
-    dummy_df.to_csv('sample_dummy_file.csv')
+    dummy_df.to_csv('sample_dummy_file_1.csv')
     print('Automating the boring work makes our lives so much better!')
     
 if __name__== "__main__":
